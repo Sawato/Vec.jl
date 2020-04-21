772414f6ca82ea57138fffe37cb1e1ccd2a2fcf4
Sawato <shusaku.sawato@dmprof.com>
2020-04-21 19:44:32 +0900

    update.

--- src/Vec.jl
+++ src/Vec.jl
@@ -54,6 +54,12 @@ abstract type VecSE{N, R} <: AbstractVec{N, R} end
 
 lerp(a::Real, b::Real, t::Real) = a + (b-a)*t
 invlerp(a::Real, b::Real, c::Real) = (c - a)/(b-a)
+Vec.abs2(a::VecE2) = a.x * a.x + a.y * a.y
+Vec.abs2(a::VecSE2) = a.x * a.x + a.y * a.y
+Vec.hypot(a::VecE2) = sqrt(a.x * a.x + a.y * a.y)
+Vec.hypot(a::VecSE2) = sqrt(a.x * a.x + a.y * a.y)
+Vec.abs(a::VecE2) = sqrt(a.x * a.x + a.y * a.y)
+Vec.abs(a::VecSE2) = sqrt(a.x * a.x + a.y * a.y)
 
 "The L2 norm squared."
 normsquared(a::AbstractVec) = sum(x^2 for x in a)
@@ -75,13 +81,6 @@ include("vecSE2.jl")
 include("geom/geom.jl")
 include("coordinate_transforms.jl")
 include("quat.jl")
-
-abs2(a::VecE2) = a.x * a.x + a.y * a.y
-abs2(a::VecSE2) = a.x * a.x + a.y * a.y
-hypot(a::VecE2) = sqrt(a.x * a.x + a.y * a.y)
-hypot(a::VecSE2) = sqrt(a.x * a.x + a.y * a.y)
-abs(a::VecE2) = sqrt(a.x * a.x + a.y * a.y)
-abs(a::VecSE2) = sqrt(a.x * a.x + a.y * a.y)
 
 Base.vec(v::Union{AbstractVec, Quat, RPY}) = convert(Vector{Float64}, v)
 
